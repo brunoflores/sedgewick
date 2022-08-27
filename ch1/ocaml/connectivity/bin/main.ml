fb707c35b29497be72b884180b124394e01b9728
Bruno Flores <bruno@brunoflores.com.br>
2022-08-28 08:48:34 +1000

    Continue.

--- ch1/ocaml/connectivity/bin/main.ml
+++ ch1/ocaml/connectivity/bin/main.ml
@@ -1,17 +1,25 @@
-let connectivity_on_lines (module M : Connectivity.Union_find.S)
-    (ch : in_channel) : unit =
-  let rec read () =
-    match In_channel.input_line ch with
-    | Some line ->
-        let pair = Scanf.sscanf line "%d %d" (fun p q -> (p, q)) in
-        (match M.find pair with
-        | Some (p, q) ->
-            M.union (p, q);
-            Printf.printf "%d %d\n" p q
-        | None -> ());
-        read ()
-    | None -> ()
-  in
-  read ()
+let run (module M : Connectivity.Union_find.S) (pairs : (int * int) list) :
+    (int * int) list =
+  List.fold_left
+    (fun acc pair ->
+      match M.find pair with
+      | Some pair ->
+          M.union pair;
+          pair :: acc
+      | None -> acc)
+    [] pairs
 
-let () = connectivity_on_lines (module Connectivity.Quickfind_array) stdin
+let () =
+  let rec input_all ch pairs : (int * int) list =
+    match In_channel.input_line ch with
+    | Some line -> (
+        try
+          let pair = Scanf.sscanf line "%d %d" (fun p q -> (p, q)) in
+          input_all ch (pair :: pairs)
+        with _ -> failwith "failed to parse")
+    | None -> pairs
+  in
+  let input = input_all stdin [] in
+  let _ = run (module Connectivity.Quickfind_array) input in
+  let _ = run (module Connectivity.Quickunion_array) input in
+  ()
