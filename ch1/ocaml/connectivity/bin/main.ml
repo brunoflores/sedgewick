3e4f3879cc6c709ecc3a658c68e915860830586f
Bruno Flores <bruno@brunoflores.com.br>
2022-08-27 18:38:40 +1000

    Read stdin.

--- ch1/ocaml/connectivity/bin/main.ml
+++ ch1/ocaml/connectivity/bin/main.ml
@@ -1,1 +1,9 @@
-let () = print_endline "Hello, World!"
+let () =
+  let rec read () =
+    match In_channel.input_line stdin with
+    | Some line ->
+        print_endline line;
+        read ()
+    | None -> ()
+  in
+  read ()
