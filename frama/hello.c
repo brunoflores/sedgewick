ff810180d1334f98f262c074ddacacda5c2af079
Bruno Flores <bruno@brunoflores.com.br>
2022-09-01 20:26:52 +1000

    Add example.

--- frama/hello.c
+++ frama/hello.c
@@ -28,6 +28,22 @@ int abs(int val) {
 }
 
 /*@
+  requires \valid(q);
+  requires \valid(r);
+  requires \separated(q, r);
+  requires y != 0;
+
+  assigns *q, *r;
+
+  ensures div: (*q) * y + (*r) == x;
+  ensures rem: (*r) < y;
+*/
+void div_rem(unsigned x, unsigned y, unsigned *q, unsigned *r) {
+  *q = x / y;
+  *r = x % y;
+}
+
+/*@
   requires \valid_read(a);
   requires *a <= INT_MAX - 5;
 
@@ -62,6 +78,11 @@ int main() {
 
   int c = abs(-42);
   //@ assert got_42: c == 42;
+
+  unsigned div = 0;
+  unsigned rem = 0;
+  div_rem(4, 2, &div, &rem);
+  //@ assert div == 2;
 
   return 0;
 }
