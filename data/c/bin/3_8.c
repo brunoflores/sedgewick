350b5fb5d085a750ba1bc607a3a1644de40da0ea
Bruno Flores <bruno@brunoflores.com.br>
2022-09-04 17:21:13 +1000

    Continue.

---
+++ data/c/bin/3_8.c
@@ -0,0 +1,30 @@
+#include "Point.h"
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+float randFloat(void) { return 1.0 * rand() / RAND_MAX; }
+
+int main(int __attribute__((unused)) argc, char *argv[]) {
+  float d = atof(argv[2]);
+  int i;
+  int j;
+  int cnt = 0;
+  int N = atoi(argv[1]);
+  point *a = malloc(N * sizeof(*a));
+
+  for (i = 0; i < N; i++) {
+    a[i].x = randFloat();
+    a[i].y = randFloat();
+  }
+
+  for (i = 0; i < N; i++) {
+    for (j = i + 1; j < N; j++) {
+      if (distance(a[i], a[j]) < d) {
+        cnt++;
+      }
+    }
+  }
+
+  printf("%d edges shorter than %f\n", cnt, d);
+}
