452988fa2276e3acd8a1e1a2bd2b0563087edf79
Bruno Flores <bruno@brunoflores.com.br>
2022-09-04 10:24:19 +1000

    Continue.

--- 2_6_search/c/bin/binary.c
+++ 2_6_search/c/bin/binary.c
@@ -1,1 +1,16 @@
+int search(int a[], int val, int left, int right) {
+  while (right >= left) {
+    int middle = (left + right) / 2;
+    if (val == a[middle]) {
+      return middle;
+    }
+    if (val < a[middle]) {
+      right = middle - 1;
+    } else {
+      left = middle + 1;
+    }
+  }
+  return -1;
+}
+
 int main() { return 0; }
--- 2_6_search/c/bin/sequential.c
+++ 2_6_search/c/bin/sequential.c
@@ -1,1 +1,22 @@
+/*
+  Linear search:
+
+  Check whether the number `v` is among a previously stored set of numbers in
+  `a[l], a[l+1], ..., a[r]`, by comparing against each number sequentially,
+  starting at the beginning.
+
+  If we reach the end without finding the number sought, then we return the
+  value `-1`. Otherwise, we return the index of the array position containing
+  the number.
+*/
+int search(int a[], int val, int left, int right) {
+  int i;
+  for (i = left; i <= right; i++) {
+    if (val == a[i]) {
+      return i;
+    }
+  }
+  return -1;
+}
+
 int main() { return 0; }
